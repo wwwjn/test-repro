18fdcf5bdd3e50e22fca68adaaba7c41a893764c
Ashwin Ramaswami <aramaswamis@gmail.com>
2022-10-13 13:57:30 -0400

    Update tst-index.py

--- tst-index.py
+++ tst-index.py
@@ -1,6 +1,4 @@
 import tempfile
-from codalab.worker.file_util import GzipStream
-
 
 import indexed_gzip
 
@@ -13,16 +11,5 @@ def test_indexed_gzip(file_path):
     tar_file = indexed_gzip.IndexedGzipFile(fileobj=output_fileobj, drop_handles=False, spacing=4*1024*1024)
     while tar_file.read(1024 * 1024):
         continue
-    # output_fileobj is not file-like 
-    # so we download it from remote cloud storage again 
-    # with tempfile.NamedTemporaryFile(suffix=".sqlite") as tmp_index_file:
-    #     SQLiteIndexedTar(
-    #         fileObject=output_fileobj,
-    #         tarFileName="contents",  # If saving a single file as a .gz archive, this file can be accessed by the "/contents" entry in the index.
-    #         writeIndex=True,
-    #         clearIndexCache=True,
-    #         indexFilePath=tmp_index_file.name,
-    #         printDebug=3,
-    #     )
 
 test_indexed_gzip(file_path)  # filepath points to a large file.
