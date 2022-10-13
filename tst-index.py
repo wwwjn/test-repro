d69abd014125c18785f8e05a113e7f423e48993f
Ashwin Ramaswami <aramaswamis@gmail.com>
2022-10-13 15:06:57 -0400

    Update tst-index.py

--- tst-index.py
+++ tst-index.py
@@ -80,11 +80,11 @@ def test_indexed_gzip(file_path):
     source_fileobj = open(file_path, 'rb')
     def fn(*args, **kwargs):
         raise io.UnsupportedOperation
-    source_fileobj.seek = fn
-    source_fileobj.tell = fn
+#     source_fileobj.seek = fn
+#     source_fileobj.tell = fn
 #     f = io.BytesIO(source_fileobj.read())
-    source_fileobj.seekable = lambda: False
-    source_fileobj.fileno = fn
+#     source_fileobj.seekable = lambda: False
+#     source_fileobj.fileno = fn
     tar_file = indexed_gzip.IndexedGzipFile(fileobj=GzipStream(source_fileobj))
     tar_file.build_full_index()
 #     while tar_file.read(1024 * 1024):
