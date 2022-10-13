5bc9e9402d55b5bc2a0e9047dd565abbc4016191
Ashwin Ramaswami <aramaswamis@gmail.com>
2022-10-13 12:24:33 -0400

    Update tst-index.py

--- tst-index.py
+++ tst-index.py
@@ -8,7 +8,7 @@ file_path = 'temp_10GB_file'
 
 def test_indexed_gzip(file_path):
     source_fileobj = open(file_path, 'rb')
-    output_fileobj = GzipStream(source_fileobj)
+    output_fileobj = source_fileobj
     # do the same thing as SQLiteIndexedTar do
     tar_file = indexed_gzip.IndexedGzipFile(fileobj=output_fileobj, drop_handles=False, spacing=4*1024*1024)
     while tar_file.read(1024 * 1024):
