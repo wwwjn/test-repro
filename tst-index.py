381c18ca80641db7718cc55f5fcaaa4483775d82
Ashwin Ramaswami <aramaswamis@gmail.com>
2022-10-13 14:34:56 -0400

    Update tst-index.py

--- tst-index.py
+++ tst-index.py
@@ -1,9 +1,16 @@
 import indexed_gzip
+import io
 
 file_path = 'temp_file.gz'
 
 def test_indexed_gzip(file_path):
     source_fileobj = open(file_path, 'rb')
+    def fn(self):
+        raise UnsupportedOperation
+    source_fileobj.seek = fn
+    source_fileobj.tell = fn
+    source_fileobj.seekable = lambda: False
+    source_fileobj.fileno = fn
     tar_file = indexed_gzip.IndexedGzipFile(fileobj=source_fileobj, drop_handles=False, spacing=4*1024*1024)
     while tar_file.read(1024 * 1024):
         continue
